0c8dcea6bce0056a286faa6797b15295cf8c0b91
rafittu <rafittu@outlook.com>
2023-08-18 16:41:21 -0300

    feat: sign in page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import SignIn from './pages/SignIn';
 
 function App() {
-  return <h1>LUMIN</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+    </Routes>
+  );
 }
 
 export default App;
