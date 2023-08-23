c76d967d6a6fc57b06529d45f5b4c851f06e5a40
rafittu <rafittu@outlook.com>
2023-08-23 11:49:12 -0300

    feat: form schema

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function SignUp() {
-  return <h1>Sign Up</h1>;
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    socialName: '',
+    username: '',
+    bornDate: '',
+    motherName: '',
+    email: '',
+    phone: '',
+    password: '',
+    passwordConfirmation: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="firstName">
+          Primeiro nome:
+          <input
+            type="text"
+            name="firstName"
+            id="firstName"
+            value={formData.firstName}
+            onChange={handleChange}
+            required
+            placeholder="primeiro nome"
+          />
+        </label>
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
 
 export default SignUp;
