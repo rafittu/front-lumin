291c81c281ce07a7c8cdf7a2aa6d6e5b4d5d265d
rafittu <rafittu@outlook.com>
2023-08-23 11:06:52 -0300

    feat: add sign up route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 
 import './style/App.css';
@@ -11,8 +12,10 @@ function App() {
     <div className="backgroundImage">
       <AuthProvider>
         <Routes>
-          <Route exact path="/" element={<SignIn />} />
           <Route exact path="/home" element={<Home />} />
+          <Route exact path="/signup" element={<SignUp />} />
+          <Route exact path="/signin" element={<SignIn />} />
+          <Route exact path="/" element={<SignIn />} />
         </Routes>
       </AuthProvider>
     </div>
