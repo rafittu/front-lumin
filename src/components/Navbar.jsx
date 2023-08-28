72a473e8003463f4b6c36d4fe3a08c4a9f5c7123
rafittu <rafittu@outlook.com>
2023-08-28 18:50:58 -0300

    feat: disable menu option

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,31 @@ import { Link } from 'react-router-dom';
 import '../style/Navbar.css';
 
 function Navbar() {
+  const user = {
+    status: 'PENDING_CONFIRMATION',
+  }; // Get user status from context created on home page
+
   return (
     <nav>
       <Link to="/home">Inicio</Link>
-      <Link to="/schedule">Agenda</Link>
-      <Link to="/clients">Clientes</Link>
-      <Link to="/records">Fichas</Link>
+      <Link
+        to="/schedule"
+        className={user.status !== 'ACTIVE' ? 'disabled' : ''}
+      >
+        Agenda
+      </Link>
+      <Link
+        to="/clients"
+        className={user.status !== 'ACTIVE' ? 'disabled' : ''}
+      >
+        Clientes
+      </Link>
+      <Link
+        to="/records"
+        className={user.status !== 'ACTIVE' ? 'disabled' : ''}
+      >
+        Fichas
+      </Link>
     </nav>
   );
 }
