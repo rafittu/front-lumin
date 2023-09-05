ba058db1737b3aafb66a5403859464ff44cf448a
rafittu <rafittu@outlook.com>
2023-09-05 17:50:50 -0300

    feat: axios call to get appointments

--- src/pages/Schedules.jsx
+++ src/pages/Schedules.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useUser } from '../contexts/UserContext';
 import Navbar from '../components/Navbar';
@@ -7,12 +7,13 @@ import '../style/Schedules.css';
 
 function Schedules() {
   const { userData } = useUser();
+  const [appointments, setAppointments] = useState([]);
   const accessToken = localStorage.getItem('accessToken');
 
   useEffect(() => {
     const fetchAppointmentsData = async () => {
       try {
-        const appointments = await axios.get(
+        const response = await axios.get(
           `http://localhost:3001/schedules/professional/${userData.id}`,
           {
             headers: {
@@ -21,21 +22,22 @@ function Schedules() {
           },
         );
 
-        console.log(appointments);
+        setAppointments(response.data.appointments);
       } catch (error) {
         console.log(error.response.data);
       }
     };
 
     fetchAppointmentsData();
-  }, []);
+  }, [accessToken, userData.id]);
 
   return (
     <section>
       <Navbar />
 
       <div className="content-container">
-        <h1>CONTENT</h1>
+        <h1>Agenda de Compromissos</h1>
+        <AppointmentCalendar appointments={appointments} />
       </div>
     </section>
   );
