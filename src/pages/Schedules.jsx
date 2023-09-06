dd5e6c13c74caa49bb6318226d37b8f1bbcf1981
rafittu <rafittu@outlook.com>
2023-09-06 16:38:30 -0300

    feat: button to create new appointment

--- src/pages/Schedules.jsx
+++ src/pages/Schedules.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 import AppointmentCalendar from '../components/AppointmentCalendar';
+import AppointmentForm from '../components/AppointmentForm';
 
 import '../style/Schedules.css';
 
 function Schedules() {
   const [appointments, setAppointments] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
   const accessToken = localStorage.getItem('accessToken');
   const userData = JSON.parse(localStorage.getItem('userData'));
 
@@ -24,7 +27,7 @@ function Schedules() {
 
         setAppointments(response.data.appointments);
       } catch (error) {
-        // console.log(error.response.data);
+        console.log(error.response.data);
       }
     };
 
@@ -38,6 +41,14 @@ function Schedules() {
       <div id="schedule-container">
         <h1>Agenda de Compromissos</h1>
         <AppointmentCalendar appointments={appointments} />
+
+        {showForm ? <AppointmentForm /> : null}
+
+        {!showForm ? (
+          <button type="button" onClick={() => setShowForm(true)}>
+            Criar Novo Compromisso
+          </button>
+        ) : null}
       </div>
     </section>
   );
