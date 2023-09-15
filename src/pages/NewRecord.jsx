9c38b95fc114ba70dada68af1dd5b4a99826ebfa
rafittu <rafittu@outlook.com>
2023-09-15 17:32:26 -0300

    feat: record form schema

--- src/pages/NewRecord.jsx
+++ src/pages/NewRecord.jsx
@@ -4,6 +4,7 @@ import { useAppointment } from '../contexts/AppointmentContext';
 
 function NewRecord() {
   const { appointmentData } = useAppointment();
+  const { clientName, clientPhone, appointmentDate } = appointmentData;
 
   if (!appointmentData) {
     return (
@@ -17,8 +18,61 @@ function NewRecord() {
     <section>
       <Navbar />
 
-      <div className="content-container">
-        <span>new record form</span>
+      <div id="record-container">
+        <h1>Novo Registro de Atendimento</h1>
+
+        <form>
+          <div>
+            <label htmlFor="clientName">
+              Nome do Cliente:
+              <input
+                type="text"
+                id="clientName"
+                name="clientName"
+                value={clientName}
+                readOnly
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="clientPhone">
+              Telefone:
+              <input
+                type="text"
+                id="clientPhone"
+                name="clientPhone"
+                value={clientPhone}
+                readOnly
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="appointmentDate">
+              Data do Atendimento:
+              <input
+                type="text"
+                id="appointmentDate"
+                name="appointmentDate"
+                value={appointmentDate}
+                readOnly
+              />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="comments">
+              Comentários:
+              <textarea
+                id="comments"
+                name="comments"
+                placeholder="Adicione seus comentários aqui..."
+                rows="4"
+              />
+            </label>
+          </div>
+          <div>
+            <button type="submit">Salvar Registro</button>
+          </div>
+        </form>
       </div>
     </section>
   );
