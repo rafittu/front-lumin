4eca66d75007c5dd09ecfedfddcc2befd7ec89f2
rafittu <rafittu@outlook.com>
2023-09-15 17:14:08 -0300

    feat: set appointment in context

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
+import { useAppointment } from '../contexts/AppointmentContext';
 
 import '../style/AppointmentDetails.css';
 
@@ -9,6 +10,7 @@ function AppointmentDetails() {
   const navigate = useNavigate();
   const { id } = useParams();
   const appointmentId = id;
+  const { setAppointmentData } = useAppointment();
 
   const [appointment, setAppointment] = useState(null);
   const [isEditing, setIsEditing] = useState(false);
@@ -33,6 +35,7 @@ function AppointmentDetails() {
         );
 
         setAppointment(response.data.appointments[0]);
+        setAppointmentData(response.data.appointments[0]);
         setEditedAppointment({ ...response.data.appointments[0] });
       } catch (error) {
         console.error(error.response.data);
