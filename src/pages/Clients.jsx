a2057f5a726b72ecba374194c60e20bd4e86df28
rafittu <rafittu@outlook.com>
2023-09-18 18:11:43 -0300

    feat: render clients list

--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Navbar from '../components/Navbar';
 
+import '../style/Clients.css';
+
 function ClientsList() {
   const accessToken = localStorage.getItem('accessToken');
   const userData = JSON.parse(localStorage.getItem('userData'));
 
   const [clients, setClients] = useState([]);
+  const [selectedClient, setSelectedClient] = useState(null);
 
   useEffect(() => {
     const fetchClients = async () => {
@@ -27,40 +30,64 @@ function ClientsList() {
     };
 
     fetchClients();
-  }, []);
+  }, [accessToken, userData.id]);
+
+  const handleClientClick = (client) => {
+    setSelectedClient(client);
+  };
 
   return (
     <section>
       <Navbar />
 
-      <div className="clients-container">
+      <div id="clients-container">
         <div id="client-list">
           <ul>
             {clients.map((client) => (
-              <li key={client.id}>{client.name}</li>
+              <li key={client.id}>
+                <button type="button" onClick={() => handleClientClick(client)}>
+                  {client.name}
+                </button>
+              </li>
             ))}
           </ul>
         </div>
 
-        <div id="client-details">
-          <span>
-            After click on a user from list, render user details (name, phone)
-            on upper center/right content-container
-          </span>
-        </div>
+        <div id="selected-client">
+          <div id="client-details">
+            {selectedClient ? (
+              <>
+                <p>
+                  <strong>Nome:</strong>
+                  {' '}
+                  {selectedClient.name}
+                </p>
+                <p>
+                  <strong>Telefone:</strong>
+                  {' '}
+                  {selectedClient.phone}
+                </p>
+              </>
+            ) : null}
+          </div>
 
-        <div id="client-appointments">
-          <span>
-            After click on a user from list, render user schedule appointments
-            on bottom center content-container
-          </span>
-        </div>
+          <div id="client-appointments">
+            {selectedClient ? (
+              <>
+                <h2>Agendamentos</h2>
+                {/* Renderize os agendamentos do cliente aqui */}
+              </>
+            ) : null}
+          </div>
 
-        <div id="client-payments">
-          <span>
-            After click on a user from list, render user payment history on
-            bottom right content-container
-          </span>
+          <div id="client-payments">
+            {selectedClient ? (
+              <>
+                <h2>Histórico de Pagamentos</h2>
+                {/* Renderize o histórico de pagamentos do cliente aqui */}
+              </>
+            ) : null}
+          </div>
         </div>
       </div>
     </section>
