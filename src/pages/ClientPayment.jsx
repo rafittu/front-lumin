83514d096782c28148a2d429c6f96407092d19b8
rafittu <rafittu@outlook.com>
2023-09-21 17:42:51 -0300

    feat: client payment page schema

---
+++ src/pages/ClientPayment.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function ClientPayment() {
+  return (
+    <section>
+      <Navbar />
+
+      <div className="payment-container">
+        <h1>Financeiro</h1>
+      </div>
+    </section>
+  );
+}
+
+export default ClientPayment;
